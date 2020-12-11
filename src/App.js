861a0c2b8e37f88c0fd00e466b6d5a3450a6c0f8
Luke Brown <me@luke.sx>
2020-12-11 12:21:25 +0000

    Added working HLS videos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ReactHlsPlayer from 'react-hls-player'
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo"/>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+
+				<h1>Example with autoplay and no controls</h1>
+				<ReactHlsPlayer
+					url='/video/output.m3u8'
+					autoplay={true}
+					controls={false}
+					width={500}
+					height={300}
+				/>
+
+				<h1>Example of Retreat East working:</h1>
+				<p>You can see progress bar in the control showing HLS loading part by part</p>
+				<ReactHlsPlayer
+					url='/video/output.m3u8'
+					autoplay={false}
+					controls={true}
+					width={500}
+					height={300}
+				/>
+
+				<h1>Example of another m3u8 video working:</h1>
+				<p>This is a 15 minute video where you can see m3u8 loading part by part</p>
+				<ReactHlsPlayer
+					url='https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8'
+					autoplay={false}
+					controls={true}
+					width={500}
+					height={280}
+				/>
 			</header>
 		</div>
 	);
